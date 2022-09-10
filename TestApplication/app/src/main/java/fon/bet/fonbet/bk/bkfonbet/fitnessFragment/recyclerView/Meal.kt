ed7fbcbfa2664e69506a21b3053e4c37f99a0695
EmilBaimurzin <emilbaimurzin@gmail.com>
2022-09-11 01:17:02 +0600

    done

--- TestApplication/app/src/main/java/fon/bet/fonbet/bk/bkfonbet/fitnessFragment/recyclerView/Meal.kt
+++
@@ -1,9 +0,0 @@
-package fon.bet.fonbet.bk.bkfonbet.fitnessFragment.recyclerView
-
-data class Meal(
-    val mealType: String,
-    val meal: String,
-    val PCF: String,
-    val totalCalories: String,
-    val image: String
-)
